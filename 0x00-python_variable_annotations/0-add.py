11fac4ac10b64b74b415eb52502385b4029ce082
Ogollah <kogollah2011@gmail.com>
2024-01-11 13:21:13 +0300

    adds two floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function for adding two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that adds two floats and returns the result.
+
+    Parameters:
+    a (float): The first float operand.
+    b (float): The second float operand.
+
+    Returns:
+    float: The sum of the two input floats.
+    """
+    return a + b
